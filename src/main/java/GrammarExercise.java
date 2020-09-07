4a445ea461ea60abc487ed19514d6e01bf43ff17
ylj <758766951@qq.com>
2020-09-07 10:54:19 +0800

    [ylj]feat:complete GrammarExercise.java.

--- src/main/java/GrammarExercise.java
+++ src/main/java/GrammarExercise.java
@@ -1,4 +1,8 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class GrammarExercise {
     public static void main(String[] args) {
@@ -6,13 +10,43 @@ public class GrammarExercise {
         String firstWordList = "";
         String secondWordList = "";
 
+        Scanner sc = new Scanner(System.in);
+        firstWordList = sc.nextLine();
+        secondWordList = sc.next();
+
         List<String> result = findCommonWordsWithSpace(firstWordList,secondWordList);
         //按要求输出到命令行
-
+        result.forEach(element->{System.out.println(element);});
     }
 
     public static List<String> findCommonWordsWithSpace(String firstWordList, String secondWordList) {
-        //在这编写实现代码
-        return null;
+        String combineStr = firstWordList.concat(secondWordList);
+        if(combineStr.contains(",,")){
+            throw new RuntimeException("input not valid");
+        }
+        List<String> combineStrList = Arrays.asList(combineStr.split(""));
+        combineStrList.forEach(element->{
+            char temp = element.charAt(0);
+            if(temp!=',' && (temp>'Z'||temp<'A') && (temp>'z'||temp<'a')){
+                throw new RuntimeException("input not valid");
+            }
+        });
+
+        firstWordList = firstWordList.toUpperCase();
+        String secondWord = secondWordList.toUpperCase();
+        //一.找到共有字符串，并去重排序
+        List<String> commonResultList = new ArrayList<>();
+        List<String> firstList = Arrays.asList(firstWordList.split(","));
+        firstList.forEach(element->{
+            if(secondWord.contains(element)){
+                commonResultList.add(element);
+            }
+        });
+        List<String> commonSortResult = new ArrayList<>();
+        commonSortResult = commonResultList.stream().distinct().sorted().collect(Collectors.toList());
+        //二.组装成字符串内部各字符间加空格
+        List<String> commonResultSortWithSpace = new ArrayList<>();
+        commonSortResult.forEach(element -> commonResultSortWithSpace.add(String.join(" ",element.split(""))));
+        return commonResultSortWithSpace;
     }
 }
